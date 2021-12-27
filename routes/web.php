a35848a3d18cbba697e3d63a0b3817d75b78e497
duongtran97 <orion.diget97@gmail.com>
2021-12-27 10:06:50 +0700

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('test');
-});
-// Route::get('/',[LoginController::class,'index']);
+// Route::get('/', function () {
+//     return view('test');
+// });
+Route::get('/',[LoginController::class,'index']);
 Route::get('/home',[HomeController::class,'index']);
 // Route::get('/login',[HomeController::class,'index']);
 Route::post('/login',[LoginController::class,'checkLogin']);
